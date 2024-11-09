4c671cd6f53b7e72db496126991fc3439898f712
FES001 <firdavs7794948@gmail.com>
2024-11-10 00:44:47 +0500

    s

--- scripts/main.js
+++ scripts/main.js
@@ -10,17 +10,6 @@ inputAddButton.addEventListener('click', e => {
     e.preventDefault();
 
     if (inputItself.value.length > 0) {
-        /* const ingredientItself = document.createElement('div');
-        ingredientItself.classList.add('ingredient-itself');
-        ingredientItself.innerHTML = `
-            <p class="ingredient-itself-name">${inputItself.value}</p>
-            <button class="ingredient-itself-button">
-                <svg class="ingredient-itself-button-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#000000" fill="none">
-                    <path d="M19.0005 4.99988L5.00049 18.9999M5.00049 4.99988L19.0005 18.9999" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
-            </button>
-        `; */
-        /* ingredientsContainer.appendChild(ingredientItself); */
         ingredientsContainer.innerHTML += `
             <div class="ingredient-itself">
                 <p class="ingredient-itself-name">${inputItself.value}</p>
@@ -51,3 +40,18 @@ inputAddButton.addEventListener('click', e => {
         popUpMessageContainer.classList.add('pop-up-error-message-active');
     };
 });
+
+
+// SEARCH
+
+inputItself.addEventListener('input', () => {
+    getFoodRecipe();
+});
+
+window.addEventListener('DOMContentLoaded', getFoodRecipe)
+
+async function getFoodRecipe() {
+    const response = await fetch('https://api.spoonacular.com/recipes/complexSearch');
+    const jsonData = await response.json();
+    console.log(jsonData);
+}
